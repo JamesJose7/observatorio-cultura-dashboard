eae241952a74c95be40f30a5b2dd243806e693f1
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-10-31 10:01:53 -0500

    Initial project

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+
+class Header extends React.Component {
+    render() {
+
+        return(
+            <header>
+                <div className="container">
+                    <h1>Header</h1>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header
