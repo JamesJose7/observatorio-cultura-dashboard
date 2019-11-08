1b67284987d96649d221a8e55444e23a65d2a742
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-11-08 15:56:03 -0500

    Changed to a local cors proxy to avoid latency and downtime

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    koboToken: 'Token 1dda84dc2ee0d5b04dcc8015bfbba66de32f080f'
+    koboToken: 'Token 1dda84dc2ee0d5b04dcc8015bfbba66de32f080f',
+    corsProxy: 'http://localhost:8080/'
+    // corsProxy: 'https://cors-anywhere.herokuapp.com/'
 }
